8479ed7854c0c2440e5c667160be28a4881f22c4
Alex Turaev <altex_76@mail.ru>
2022-09-14 22:03:00 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Boolean res = LoginService.checkLoginFields("login111111111111111111111", "pass", "pass");
+        System.out.println("Результат проверки вводных данных = " + res);
     }
 }
